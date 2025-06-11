381d98d7b1596e2e3fb88e67048bc1243fe79a32
ganu <ganeshchaudhari9353@gmail.com>
2025-06-11 15:44:07 +0530

    add sidebar

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,18 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
+// Create user
 exports.createUser = (name, email, hashedPassword, role) => {
   return new Promise((resolve, reject) => {
     const sql = "INSERT INTO users (name, username, password, role) VALUES (?, ?, ?, ?)";
     db.query(sql, [name, email, hashedPassword, role], (err, result) => {
       if (err) reject(err);
-      else resolve(result); // result.insertId will be used
+      else resolve(result);
     });
   });
 };
 
-
+// Find user by email
 exports.findUserByEmail = (email, callback) => {
-  const sql = 'SELECT * FROM users WHERE email = ?';
+  const sql = "SELECT * FROM users WHERE username = ?";
   db.query(sql, [email], callback);
 };
